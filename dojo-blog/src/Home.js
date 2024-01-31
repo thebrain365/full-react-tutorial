37c9329a1ca11fe0c8317ccee546eed470b3f03b
Muano <muano@psybergate.co.za>
2024-01-31 13:43:10 +0200

    added a functionality to list blogs

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: "Spring and Spring Boot", body: "lorem-ipsum...", author: "Muano", id: 1},
+        {title: "React", body: "lorem-ipsum...", author: "Makhokha", id: 2},
+        {title: "Android Studio", body: "lorem-ipsum...", author: "Ano", id: 3}
+    ])
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            { blogs.map((blog) => (
+                <div className="blog-preview" key={ blog.id }>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author }</p>
+                </div>
+            )) }
         </div>
      );
 }
