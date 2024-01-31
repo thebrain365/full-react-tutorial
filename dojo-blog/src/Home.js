b34eafb878687597769c36fbdc8c0fc4dcfb373a
Muano <muano.m365@gmail.com>
2024-01-31 19:49:54 +0200

    added the delete functionality

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,16 +2,20 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs] = useState([
+    const [blogs, setBlogs] = useState([
         {title: "Spring and Spring Boot", body: "lorem-ipsum...", author: "Muano", id: 1},
         {title: "React", body: "lorem-ipsum...", author: "Makhokha", id: 2},
         {title: "Android Studio", body: "lorem-ipsum...", author: "Muano", id: 3}
     ])
 
+    const handleDelete = id => {
+        let newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="home">
-            <BlogList blogs={ blogs } title="All Blogs"></BlogList>
-            <BlogList blogs={ blogs.filter(blog => blog.author === "Muano") } title="Muano's Blogs"></BlogList>
+            <BlogList blogs={ blogs } handleDelete={handleDelete} title="All Blogs"></BlogList>
         </div>
      );
 }
