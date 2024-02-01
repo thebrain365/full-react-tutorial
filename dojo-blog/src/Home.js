b95d90acfa7897b788203e0c8150074c0f3feaca
Muano <muano.m365@gmail.com>
2024-02-01 09:44:58 +0200

    added the error functionality

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,18 +4,32 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-        .then(res => { return res.json(); })
-        .then(data => {
-            setBlogs(data);
-            setIsPending(false);
-        });
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+            .then(res => { 
+                if (!res.ok) {
+                    throw Error("couldn't fetch the resource");
+                }
+                return res.json(); 
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            });
+        }, 1500)
     }, [])
 
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
             { isPending && <div>Loading blogs...</div> }
             { blogs && <BlogList blogs={ blogs } title="All Blogs"></BlogList> }
         </div>
