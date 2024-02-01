527bb83211df3b9da9656e8eb78d0c649b3c6cdf
Muano <muano.m365@gmail.com>
2024-02-01 20:58:44 +0200

    created a BlogDetails component and has a route with dynamic value in it

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -8,13 +9,16 @@ function App() {
     <Router>
       <div className="App">
         <Navbar></Navbar>
-        <div className="component">
+        <div className="content">
           <Switch>
             <Route exact path="/">
               <Home></Home>
             </Route>
             <Route path="/create">
               <Create></Create>
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails></BlogDetails>
             </Route>
           </Switch>
         </div>
