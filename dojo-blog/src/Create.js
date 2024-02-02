9230bbd1bb5036f9aebba4524a5d8c6110d22458
Muano <muano.m365@gmail.com>
2024-02-02 08:42:35 +0200

    added a form to create a new blog

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,38 @@
+import { tab } from "@testing-library/user-event/dist/tab";
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("muano");
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input
+                type="text"
+                required
+                value={ title }
+                onChange={ (e) => setTitle(e.target.value) }
+                />
+                <label>Blog body:</label>
+                <textarea
+                required
+                value={ body }
+                onChange={ (e) => setBody(e.target.value) }
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                value={ author }
+                onChange={ (e) => setAuthor(e.target.value) }
+                >
+                    <option value="muano">Muano</option>
+                    <option value="makhokha">Makhokha</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
